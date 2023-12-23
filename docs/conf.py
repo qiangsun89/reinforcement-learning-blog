401865a0dcb2ca077a15854a8e8eb52f0dff04cf
Qiang Sun <sunqiang891103@gmail.com>
2023-12-23 18:31:50 +0800

    fix

--- docs/conf.py
+++ docs/conf.py
@@ -52,7 +52,7 @@ extensions = [
 ]
 
 # Add any paths that contain templates here, relative to this directory.
-templates_path = ['reinforcement-learningtemplates']
+templates_path = ['_templates']
 
 # The suffix(es) of source filenames.
 # You can specify multiple suffix as a list of string:
